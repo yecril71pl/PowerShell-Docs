115c8af8b33d066512686c967a15b24dd438676c
Christopher Yeleighton <ne01026@shark.2a.pl>
2020-08-03 21:39:49 +0200

    Add build

--- .github/workflows/test-me.ps1
+++ .github/workflows/test-me.ps1
@@ -1,4 +1,6 @@
 #!/usr/bin/env pwsh
 Set-PSRepository PSGALLERY -I:Trusted
 INSTALL-MODULE PESTER
-INVOKE-PESTER @ARGS -EnableExit
+[STRING] $BUILDROOT = $ARGS[0]
+& "$BUILDROOT/build"
+INVOKE-PESTER (JOIN-PATH $BUILDROOT 'tests/Pester') -EnableExit
