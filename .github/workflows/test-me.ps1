573e8aff715fbf3f376a34ce23a9dcfc96a78ee4
Christopher Yeleighton <ne01026@shark.2a.pl>
2020-08-03 21:44:49 +0200

    INSTALL-MODULE PLATYPS

--- .github/workflows/test-me.ps1
+++ .github/workflows/test-me.ps1
@@ -1,6 +1,6 @@
 #!/usr/bin/env pwsh
 Set-PSRepository PSGALLERY -I:Trusted
-INSTALL-MODULE PESTER
+INSTALL-MODULE PLATYPS, PESTER
 [STRING] $BUILDROOT = $ARGS[0]
 & "$BUILDROOT/build"
 INVOKE-PESTER (JOIN-PATH $BUILDROOT 'tests/Pester') -EnableExit
