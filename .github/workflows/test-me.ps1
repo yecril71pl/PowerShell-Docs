12f8308464c6b466a6adf8c7168c5a09b1b8700d
Christopher Yeleighton <ne01026@shark.2a.pl>
2020-08-04 18:11:24 +0200

    Bad UPDATE-HELP with version table

--- .github/workflows/test-me.ps1
+++ .github/workflows/test-me.ps1
@@ -1,5 +1,8 @@
 #!/usr/bin/env pwsh
 $VerbosePreference = [System.Management.Automation.ActionPreference]:: Continue
+$PSVERSIONTABLE
+UPDATE-HELP MICROSOFT.POWERSHELL.CORE -SO:NULL
+EXIT $?
 Set-PSRepository PSGALLERY -I:Trusted
 INSTALL-MODULE PLATYPS, PESTER
 [STRING] $BUILDROOT = $ARGS[0]
